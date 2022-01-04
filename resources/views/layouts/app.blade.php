e698dbda38dd2e6605248f047eaf190049843674
Usuario-Desconocido <diazdesconocido0510@gmail.com>
2022-01-04 02:26:38 -0500

    More Bugfixes

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -20,6 +20,7 @@
     </head>
     <body class="font-sans antialiased">
         <x-jet-banner />
+
         <div class="min-h-screen bg-gray-100">
             @livewire('navigation')
 
--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -4,7 +4,7 @@
 
         <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
             @foreach ($posts as $post)
-                <article class="w-full bg-center bg-cover h-80 @if($loop->first) md:col-span-2 @endif" style="background-image: url( @if($post->image) {{ Storage::url($post->image->url) }} @endif )">
+                <article class="w-full bg-center bg-cover h-80 @if($loop->first) md:col-span-2 @endif" style="background-image: url( @if($post->image) {{ Storage::url($post->image->url) }} @else  https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fwww.solofondos.com%2Fwp-content%2Fuploads%2F2016%2F02%2FFondos-wallpapers-4k.jpg&f=1&nofb=1 @endif )">
                     <div class="w-full h-full px-8 flex flex-col justify-center">
 
                         <div>
