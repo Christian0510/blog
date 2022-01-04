b49eb7366f0cce068aff251b2e40500e6bd4272a
Usuario-Desconocido <diazdesconocido0510@gmail.com>
2022-01-04 05:32:26 -0500

    .

--- app/Http/Controllers/Admin/PostController.php
+++ app/Http/Controllers/Admin/PostController.php
@@ -46,7 +46,7 @@ class PostController extends Controller
         $post = Post::create($request->all());
 
         if ($request->file('file')){
-            $url = Storage::put('storage/posts', $request->file('file'));
+            $url = Storage::put('posts', $request->file('file'));
 
             $post->image()->create([
                 'url' => $url
